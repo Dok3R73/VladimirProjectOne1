20cb0c143d34a35869cae9fa3dc696edb6f9603c
Dok3R73 <doker2273@gmail.com>
2023-02-10 13:20:24 +0400

    test git

---
+++ src/main/java/com/example/vladimirprojectone/VladimirProjectOneApplication.java
@@ -0,0 +1,29 @@
+package com.example.vladimirprojectone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@SpringBootApplication
+public class VladimirProjectOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VladimirProjectOneApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public List<String> hello() {
+        List<String> x = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            x.add("Hello");
+        }
+        return x;
+    }
+
+}
---
+++ src/test/java/com/example/vladimirprojectone/VladimirProjectOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vladimirprojectone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VladimirProjectOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
