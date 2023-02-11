86e0845d42bf8e201bb55022ff54fa45ed4a03d6
Maksim Sunoplya <maksim.sunoplya@nordclan.com>
2023-02-11 14:34:49 +0400

    Добавил пакет controller, перенес в него эндпоинты

--- src/main/java/com/example/vladimirprojectone/VladimirProjectOneApplication.java
+++ src/main/java/com/example/vladimirprojectone/VladimirProjectOneApplication.java
@@ -2,32 +2,11 @@ package com.example.vladimirprojectone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-
-@RestController
 @SpringBootApplication
 public class VladimirProjectOneApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VladimirProjectOneApplication.class, args);
-    }
-
-    @GetMapping("/hello")
-    public List<String> hello() {
-        List<String> list = new ArrayList<>();
-
-        for (int i = 0; i < 10; i++) {
-            list.add("Hello");
-        }
-        return list;
-    }
-
-    @GetMapping("/test")
-    public String test() {
-        return "Test method";
     }
 }
